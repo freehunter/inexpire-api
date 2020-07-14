1a769de4fc68d6976797ba1a7827b3aa3d466f66
Mark Hunt <thefreehunter@gmail.com>
2020-07-14 00:38:38 +0000

    okay

---
+++ create.js
@@ -0,0 +1,21 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDB from "./libs/dynamodb-lib";
+
+
+export const main = handler(async(event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      itemId: uuid.v1(),
+      content: data.content,
+      createdAt: Date.now()
+    }
+  };
+
+  await dynamoDB.put(params);
+
+  return params.Item;
+});
