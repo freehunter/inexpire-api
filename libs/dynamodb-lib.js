efe078e70769df58f5d0429e3920e0c6879a3653
Mark Hunt <thefreehunter@gmail.com>
2020-07-18 03:10:53 +0000

    adding AWS Xray

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,11 +1,12 @@
-import AWS from "aws-sdk";
+const AWSXRay = require('aws-xray-sdk-core');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 const client = new AWS.DynamoDB.DocumentClient();
 
 export default {
-    get    : (params) => client.get(params).promise(),
-    put    : (params) => client.put(params).promise(),
-    query  : (params) => client.query(params).promise(),
-    update : (params) => client.update(params).promise(),
-    delete : (params) => client.delete(params).promise(),
+    get: (params) => client.get(params).promise(),
+    put: (params) => client.put(params).promise(),
+    query: (params) => client.query(params).promise(),
+    update: (params) => client.update(params).promise(),
+    delete: (params) => client.delete(params).promise(),
 };
